992b5b6a5538953d81a895168fae868a683f3ccb
ハルユキ <jumpy12359@gmail.com>
2021-01-28 01:04:38 +1100

    Add files via upload

---
+++ MirrorSwitchSingle.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MirrorSwitchSingle : UdonSharpBehaviour
+{
+    public GameObject Mirror;
+    public GameObject offSwitch;
+    public GameObject onSwitch;
+
+    public override void Interact()
+    {
+        if (Mirror.activeSelf) // If Mirror is on
+        {
+            Mirror.SetActive(false);
+            offSwitch.SetActive(true);
+            onSwitch.SetActive(false);
+        }
+        else // If Mirror is off
+        {
+            Mirror.SetActive(true);
+            offSwitch.SetActive(false);
+            onSwitch.SetActive(true);
+        }
+    }
+}
